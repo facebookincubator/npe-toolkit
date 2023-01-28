4051c2103e5d7453f132846bfd1baed00597df54
uidude <22799484+uidude@users.noreply.github.com>
2023-01-27 18:37:01 -0800

    Smokey and the Bandit - CI smoketest, first try

---
+++ tools/smoketest/smoke.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Script smoketest the repo. Right now just does yarn install and tsc
+
+yarn install
+
+ROOT=$(cd $PWD/../../ && echo $PWD)
+echo $ROOT
+
+ln -snf $ROOT $ROOT/templates/npe-toolkit
+
+echo Calling \`yarn install\` on all directories
+cd $ROOT/templates/faves/project && yarn install
+cd $ROOT/templates/faves/server/functions && yarn install
+cd $ROOT/shell/latest && yarn install
+cd $ROOT/shell/latest/server && yarn install
+
+echo Typechecking all directories
+cd $ROOT/tools/smoketest
+yarn tsc -p $ROOT/templates/faves/client --noEmit
+yarn tsc -p $ROOT/templates/faves/admin --noEmit
+yarn tsc -p $ROOT/templates/faves/server/functions --noEmit
+yarn tsc -p $ROOT/shell/latest --noEmit
+
+
