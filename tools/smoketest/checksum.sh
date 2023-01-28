a3ca5212941496cb42af27076096981f4a271eba
uidude <22799484+uidude@users.noreply.github.com>
2023-01-27 19:25:33 -0800

    Use shashum instead of md5

--- tools/smoketest/checksum.sh
+++ tools/smoketest/checksum.sh
@@ -4,17 +4,15 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 #
-# Checksum yarn.lock files for smoke test cache
+# Checksum yarn.lock files for smoke test cache key
 
 BASEDIR=$(cd $1 && echo $PWD)
-echo Creating checksum at /tmp/tkcheksum.txt for $BASEDIR
+echo Creating checksums at /tmp/tkcheksum.txt for $BASEDIR
 
-md5 -q $BASEDIR/tools/smoketest/yarn.lock \
+shasum -q $BASEDIR/tools/smoketest/yarn.lock \
   $BASEDIR/templates/faves/project/yarn.lock \
   $BASEDIR/templates/faves/server/functions/yarn.lock \
   $BASEDIR/shell/latest/yarn.lock \
-  $BASEDIR/shell/latest/server/yarn.lock > /tmp/tkcheksumfile.txt
-
-md5 -q /tmp/tkcheksumfile.txt > /tmp/tkcheksum.txt
+  $BASEDIR/shell/latest/server/yarn.lock > /tmp/tkcheksum.txt
 
 
