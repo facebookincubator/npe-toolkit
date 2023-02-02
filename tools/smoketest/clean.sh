94e482250b9501490259193f8b329637120dc80e
uidude <22799484+uidude@users.noreply.github.com>
2023-02-02 14:10:46 -0800

    Script to clean all node_modules

---
+++ tools/smoketest/clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Clean all of the generated files in the repo
+
+SCRIPTDIR=$(dirname $0)
+BASEDIR=$(cd $SCRIPTDIR/../.. && echo $PWD)
+
+echo Clearing all generated files
+rm -rf $BASEDIR/node_modules
+rm -rf $BASEDIR/tools/smoketest/node_modules 
+rm -rf $BASEDIR/templates/faves/project/node_modules 
+rm -rf $BASEDIR/templates/faves/server/functions/node_modules 
+rm -rf $BASEDIR/shell/lates/node_modules
+rm -rf $BASEDIR/shell/latest/server/node_modules 
+rm -rf $BASEDIR/shell/latest/ios/Pods
+
+
