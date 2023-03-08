eac64d1abfae36208c22159cdaac24fd02cf8c44
uidude <22799484+uidude@users.noreply.github.com>
2023-03-07 18:41:34 -0800

    Add prettier support for project template

---
+++ templates/faves/.prettierrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  "arrowParens": "avoid",
+  "bracketSameLine": true,
+  "bracketSpacing": false,
+  "requirePragma": true,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "overrides": [
+    {
+      "files": [
+        "**/*.{cjs,mjs,js,jsx,flow}"
+      ],
+      "options": {
+        "parser": "flow"
+      }
+    },
+    {
+      "files": [
+        "**/*.graphql"
+      ],
+      "options": {
+        "parser": "graphql",
+        "requirePragma": false
+      }
+    },
+    {
+      "files": [
+        "**/*.json"
+      ],
+      "options": {
+        "parser": "json-stringify",
+        "requirePragma": false
+      }
+    },
+    {
+      "files": [
+        "**/*.{md,markdown}"
+      ],
+      "options": {
+        "parser": "markdown",
+        "proseWrap": "always",
+        "requirePragma": false
+      }
+    },
+    {
+      "files": [
+        "**/*.{ts,tsx}"
+      ],
+      "options": {
+        "parser": "typescript"
+      }
+    }
+  ],
+  "importOrderParserPlugins": ["typescript", "jsx", "decorators-legacy"],
+  "importOrderSeparation": false,
+  "importOrderSortSpecifiers": true,
+  "importOrder": ["^(react|react-native)$", "<THIRD_PARTY_MODULES>", "@toolkit/(.*)$", "@app/(.*)$", "^[./]"]
+}
