f7d86b6c08e1e115f7d07a906b1bd733f4e6f1ea
uidude <22799484+uidude@users.noreply.github.com>
2023-04-06 20:12:22 -0700

    Update prettier to ignore .d.ts files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,58 +1,57 @@
 module.exports = {
-  "arrowParens": "avoid",
-  "bracketSameLine": true,
-  "bracketSpacing": false,
-  "requirePragma": false,
-  "singleQuote": true,
-  "trailingComma": "all",
-  "overrides": [
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  requirePragma: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  overrides: [
     {
-      "files": [
-        "**/*.{cjs,mjs,js,jsx,flow}"
-      ],
-      "options": {
-        "parser": "flow"
-      }
+      files: ['**/*.{cjs,mjs,js,jsx,flow}'],
+      options: {
+        parser: 'flow',
+      },
     },
     {
-      "files": [
-        "**/*.graphql"
-      ],
-      "options": {
-        "parser": "graphql",
-        "requirePragma": false
-      }
+      files: ['**/*.graphql'],
+      options: {
+        parser: 'graphql',
+      },
     },
     {
-      "files": [
-        "**/*.json"
-      ],
-      "options": {
-        "parser": "json-stringify",
-        "requirePragma": false
-      }
+      files: ['**/*.json'],
+      options: {
+        parser: 'json-stringify',
+      },
     },
     {
-      "files": [
-        "**/*.{md,markdown}"
-      ],
-      "options": {
-        "parser": "markdown",
-        "proseWrap": "always",
-        "requirePragma": false
-      }
+      files: ['**/*.{md,markdown}'],
+      options: {
+        parser: 'markdown',
+        proseWrap: 'always',
+      },
     },
     {
-      "files": [
-        "**/*.{ts,tsx}"
-      ],
-      "options": {
-        "parser": "typescript"
-      }
-    }
+      files: ['**/*.{ts,tsx}'],
+      options: {
+        parser: 'typescript',
+      },
+    },
+    {
+      files: ['**/*.d.ts'],
+      options: {
+        requirePragma: true,
+      },
+    },
   ],
-  "importOrderParserPlugins": ["typescript", "jsx", "decorators-legacy"],
-  "importOrderSeparation": false,
-  "importOrderSortSpecifiers": true,
-  "importOrder": ["^(react|react-native)$", "<THIRD_PARTY_MODULES>", "@toolkit/(.*)$", "@app/(.*)$", "^[./]"]
-}
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrder: [
+    '^(react|react-native)$',
+    '<THIRD_PARTY_MODULES>',
+    '@toolkit/(.*)$',
+    '@app/(.*)$',
+    '^[./]',
+  ],
+};
